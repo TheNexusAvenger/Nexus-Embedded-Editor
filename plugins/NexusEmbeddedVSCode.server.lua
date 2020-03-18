658f353e9c60635036e0a36520028cabeb8c58aa
TheNexusAvenger <zpcook@googlemail.com>
2020-03-17 20:04:28 -0400

    Added VS Code plugin icons.

--- plugins/NexusEmbeddedVSCode.server.lua
+++ plugins/NexusEmbeddedVSCode.server.lua
@@ -4,4 +4,4 @@ TheNexusAvenger
 Initializes Nexus Embedded VS Code.
 --]]
 
-require(script:WaitForChild("NexusEmbeddedEditor"))(plugin,35871,"VS Code","","")
+require(script:WaitForChild("NexusEmbeddedEditor"))(plugin,35871,"VS Code","http://www.roblox.com/asset/?id=4790867149","http://www.roblox.com/asset/?id=4790887903")
